3fbe7eb1a294c2aa3381b9ff692afddc6261e089
Chris Moore <cdmwebs@gmail.com>
2010-08-06 17:23:57 -0400

    pretty catch phrases

--- bs.rb
+++ bs.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'faker'
 require 'haml'
+require 'active_support'
 
 get '/' do
-  @bs = "#{[Faker::Company.catch_phrase, Faker::Company.bs].join(" will ")}."
+  @bs = "#{[Faker::Company.catch_phrase.titleize, Faker::Company.bs].join(" will ")}."
   haml :index
 end
